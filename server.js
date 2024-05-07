f82a244f4d23bd43240a715e00a187757ee6054d
MudireddyMeghanath <105875980+MudireddyMeghanath@users.noreply.github.com>
2024-05-07 11:04:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,11 +40,12 @@ mongoose
 //   next();
 // });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Origin', 'https://front-1-8gxj.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 
 app.use(express.json());
 app.use("/users", users);
